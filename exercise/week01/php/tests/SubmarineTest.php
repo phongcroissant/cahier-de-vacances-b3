a38b452d770d73907c2d4ad38800cf105d5a4f4c
Phong NGUYEN <phongnguyen25000@gmail.com>
2025-08-10 20:53:01 +0200

    week 01 php avec test unitaire

--- exercise/week01/php/tests/SubmarineTest.php
+++ exercise/week01/php/tests/SubmarineTest.php
@@ -1,6 +1,21 @@
 <?php
 
+
 test('should move on given text instructions', function () {
-    // TODO: Implement the submarine logic and replace this placeholder value
-    expect(43)->toBe(42, 'it is universal answer');
+    $horizontal=0;
+    $depth=0;
+    $instructions=file(__DIR__ .'/submarine.txt', FILE_IGNORE_NEW_LINES);
+
+    foreach ($instructions as $instruction) {
+        if (str_contains($instruction,'forward')) {
+            $horizontal=$horizontal+intval(substr($instruction,-1));
+        }
+        if (str_contains($instruction,'down')) {
+            $depth=$depth+intval(substr($instruction,-1));
+        }
+        if (str_contains($instruction,'up')) {
+            $depth=$depth-intval(substr($instruction,-1));
+        }
+    }
+    expect($depth*$horizontal)->toBe(1690020, 'it is universal answer');
 });
---
+++ exercise/week01/php/tests/submarine.php
@@ -0,0 +1,21 @@
+<?php
+
+$horizontal=0;
+$depth=0;
+$instructions=file('submarine.txt', FILE_IGNORE_NEW_LINES);
+
+// foreach ($instructions as $instruction) {
+//     if (str_contains($instruction,'forward')) {
+//         $horizontal=$horizontal+intval(substr($instruction,-1));
+//     }
+//     if (str_contains($instruction,'down')) {
+//         $depth=$depth+intval(substr($instruction,-1));
+//     }
+//     if (str_contains($instruction,'up')) {
+//         $depth=$depth-intval(substr($instruction,-1));
+//     }
+// }
+// echo $horizontal.PHP_EOL;
+// echo $depth.PHP_EOL;
+// echo $horizontal*$depth;
+var_dump ($instructions);
