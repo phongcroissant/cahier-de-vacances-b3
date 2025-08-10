fe02e8741e0c6b9944970ceb3b652e0a15f8715b
Phong NGUYEN <phongnguyen25000@gmail.com>
2025-08-10 20:59:19 +0200

    t

--- exercise/week01/php/tests/submarine.php
+++ exercise/week01/php/tests/submarine.php
@@ -4,18 +4,18 @@ $horizontal=0;
 $depth=0;
 $instructions=file('submarine.txt', FILE_IGNORE_NEW_LINES);
 
-// foreach ($instructions as $instruction) {
-//     if (str_contains($instruction,'forward')) {
-//         $horizontal=$horizontal+intval(substr($instruction,-1));
-//     }
-//     if (str_contains($instruction,'down')) {
-//         $depth=$depth+intval(substr($instruction,-1));
-//     }
-//     if (str_contains($instruction,'up')) {
-//         $depth=$depth-intval(substr($instruction,-1));
-//     }
-// }
-// echo $horizontal.PHP_EOL;
-// echo $depth.PHP_EOL;
-// echo $horizontal*$depth;
+foreach ($instructions as $instruction) {
+    if (str_contains($instruction,'forward')) {
+        $horizontal=$horizontal+intval(substr($instruction,-1));
+    }
+    if (str_contains($instruction,'down')) {
+        $depth=$depth+intval(substr($instruction,-1));
+    }
+    if (str_contains($instruction,'up')) {
+        $depth=$depth-intval(substr($instruction,-1));
+    }
+}
+echo $horizontal.PHP_EOL;
+echo $depth.PHP_EOL;
+echo $horizontal*$depth;
 var_dump ($instructions);
